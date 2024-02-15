8aac6239f5795842ed713bd56f15ca9a0df0da8d
PavelRost <92044501+PavelRost@users.noreply.github.com>
2024-02-15 16:10:44 +0300

    add: annotations lombok for tests

--- school/src/main/java/org/rostfactory/school/dto/DriverLicenseDtoRequestCreate.java
+++ school/src/main/java/org/rostfactory/school/dto/DriverLicenseDtoRequestCreate.java
@@ -1,8 +1,10 @@
 package org.rostfactory.school.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class DriverLicenseDtoRequestCreate {
     private Long citizenId;
     private String value;
--- school/src/main/java/org/rostfactory/school/entity/DriverLicense.java
+++ school/src/main/java/org/rostfactory/school/entity/DriverLicense.java
@@ -1,12 +1,18 @@
 package org.rostfactory.school.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.SQLDelete;
 
 @Entity
 @Table(name="school")
 @SQLDelete(sql = "UPDATE school SET deleted = true WHERE id=?")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Data
 public class DriverLicense {
 
